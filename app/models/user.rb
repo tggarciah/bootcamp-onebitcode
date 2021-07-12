fb53c282804e562494ef19e2f9003a07f9e41925
Thiago Gonçalves Garcia <tggarciah@gmail.com>
2021-07-11 22:23:23 -0300

    Add search by name and pagination in the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
   validates :name, presence: true
   validates :profile, presence: true
 
   enum profile: { admin: 0, client: 1 }
+
+  include NameSearchable
+  include Paginatable
 end
--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     code { Faker::Commerce.unique.promotion_code(digits: 6) }
     status { %i(active inactive).sample }
     discount_value { rand(1.0..99.0).round(2) }
-    due_date { '2021-07-08 11:34:09' }
+    due_date { Time.zone.now + 1.day }
   end
 end
